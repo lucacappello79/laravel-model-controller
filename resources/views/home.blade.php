ca1d8e50f81fc7ed1d3ce96e8bc64796f6bd9906
lucacappello79 <postt79@gmail.com>
2023-05-10 17:38:49 +0200

    feat: styling cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends ('layouts/main')
+{{-- @extends ('layouts/main')
 
 @section('content')
 <h1>
@@ -18,5 +18,34 @@
         </div>
     </div>      
     @endforeach
+
+@endsection --}}
+
+@extends ('layouts/main')
+
+@section('content')
+<h1>
+    Elenco Film:
+</h1>
+
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $item)
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                <h5 class="card-title">{{$item->title}}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{$item->original_title}}</h6>
+                <p class="card-text">{{$item->nationality}}</p>
+                <p class="card-text">{{$item->date}}</p>
+                <p class="card-text">{{$item->vote}}</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>      
+        @endforeach
+    </div>
+</div>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
 <header>
-    <h1>ciao</h1>
+    <h1>header</h1>
 </header>
