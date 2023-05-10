56df56ae2878879eefacd8cf3b3ac42b08851536
lucacappello79 <postt79@gmail.com>
2023-05-10 18:02:10 +0200

    feat: adding footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,23 @@
-<footer>
-   <h1>Footer</h1>
+<footer class="bg-primary text-white py-4 mt-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h5>Movie DB</h5>
+                <p>Some description about your website.</p>
+            </div>
+            <div class="col-md-6">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Home</a></li>
+                    <li><a href="#" class="text-white">Features</a></li>
+                    <li><a href="#" class="text-white">Pricing</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0">&copy; {{ date('Y') }} My Website. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
 </footer>
